65087e73490ef2a7266320f49f79688ca8d4af83
AsmodeusXI <AsmodeusXI@gmail.com>
2014-04-18 22:46:52 -0500

    Adding some basic functions to create the UI needs.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+function createMatchDay() {
+	var matchDayElement = document.getElementById("matchDay");
+	createDateInput(matchDayElement, 'matchDay');
+	var matchButton = document.createElement('button');
+	matchButton.innerHTML = "Create Match";
+	matchButton.className = 'matchDay';
+	matchButton.onclick = function() { createMatch(); }; 
+	matchDayElement.appendChild(matchButton);
+}
+
+function createDateInput(appendElement, elementClass) {
+	var dateInput = document.createElement('input');
+	dateInput.type = 'date';
+	dateInput.className = elementClass;
+	appendElement.appendChild(dateInput);
+}
+
 function createMatch() {
-	alert("It's a day!");
+	alert("Making a match!");
 }
