7207a52ea2874aa86cacecde857fc7d2df81b036
AsmodeusXI <AsmodeusXI@gmail.com>
2014-05-08 22:02:26 -0500

    Final spoiler section creation and polishing of both code and (basic) CSS.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+/* 
+Image variables for teams (should be more dynamic)
+*/
 var TSMimg = 'http://i.imgur.com/U5YbY2u.png';
 var CLGimg = 'http://i.imgur.com/vn4ZQES.png';
 var C9img = 'http://i.imgur.com/kfEe9Gx.png';
@@ -7,13 +10,15 @@ var DIGimg = 'http://i.imgur.com/IErbRST.png';
 var EGimg = 'http://i.imgur.com/9YA9ffr.png';
 var CSTimg = 'http://i.imgur.com/jLDb8ge.png';
 
-// Create a match day 
+/*
+Create a match day 
+*/
 function createMatchDay() {
 	
 	// Create matchDay div
 	var matchDayElement = document.createElement('div');
 	matchDayElement.className = 'matchDay';
-	$(matchDayElement).append('<br>');
+	$(matchDayElement).append('<h3>Match Day</h3>');
 
 	// Add the date and time inserts
 	createDateInput(matchDayElement, 'matchDayDateTime');
@@ -32,7 +37,9 @@ function createMatchDay() {
 	$('body').append(matchDayElement);
 }
 
-// Create a date input for a match day
+/* 
+Create a date input for a match day
+*/
 function createDateInput(appendElement, elementClass) {
 	
 	// Create date control
@@ -58,12 +65,14 @@ function createDateInput(appendElement, elementClass) {
 	appendElement.appendChild(timeZoneInput);
 }
 
-// Creates a match in a match day (between two teams)
+/*
+Creates a match in a match day (between two teams)
+*/
 function createMatch(appendElement, elementClass) {
 	var matchDiv = document.createElement('div');
 	matchDiv.className = 'match';
-	//matchDiv.id = 
 	$(appendElement).append(matchDiv);
+	$(matchDiv).append('<h4>Match</h4>')
 	$(matchDiv).append('Team 1: ');
 	$(matchDiv).append(createTeamDropdown(1));
 	$(matchDiv).append('<br>');
@@ -80,23 +89,20 @@ function createMatch(appendElement, elementClass) {
 	addRemoveButton(matchDiv, 'Remove Match', 'Match');
 }
 
-// Creates a team dropdown (currently only NA teams)
+/*
+Creates a team dropdown (currently only NA teams)
+*/
 function createTeamDropdown(teamNo) {
 	var ddName = "team" + teamNo;
 	var teamDropdown = document.createElement('select');
 	teamDropdown.id = ddName;
-	$(teamDropdown).append(createTeamOption('TSM',TSMimg));
-	$(teamDropdown).append(createTeamOption('CLG',CLGimg));
-	$(teamDropdown).append(createTeamOption('C9',C9img));
-	$(teamDropdown).append(createTeamOption('XDG',XDGimg));
-	$(teamDropdown).append(createTeamOption('EG',EGimg));
-	$(teamDropdown).append(createTeamOption('DIG',DIGimg));
-	$(teamDropdown).append(createTeamOption('CST',CSTimg));
-	$(teamDropdown).append(createTeamOption('CRS',CRSimg));
+	addNATeams(teamDropdown);
 	return teamDropdown;
 }
 
-// Adds a team option to a team select dropdown
+/*
+Adds a team option to a team select dropdown
+*/
 function createTeamOption(team, image) {
 	var teamOption = document.createElement('option');
 	teamOption.value = image;
@@ -104,7 +110,9 @@ function createTeamOption(team, image) {
 	return teamOption;
 }
 
-// Adds a "remove surrounding element" button. Used for match days and matches.
+/*
+Adds a "remove surrounding element" button. Used for match days and matches.
+*/
 function addRemoveButton(appendElement, removeText, removeType) {
 	var removeButton = document.createElement('button');
 	removeButton.innerHTML = removeText;
@@ -113,10 +121,12 @@ function addRemoveButton(appendElement, removeText, removeType) {
 	$(appendElement).append(removeButton);
 }
 
-// Uses the assembled information to create a TL BB code LR
+/*
+Uses the assembled information to create a TL BB code LR
+*/
 function createLR() {
 	$('body').append('<br>');
-	$('body').append( '<textarea name="LRpaste" rows="40" cols="100"></textarea>' );
+	$('body').append( '<textarea name="LRpaste" id="LRpaste" rows="40" cols="100"></textarea>' );
 	var LRtext = $("textarea[name='LRpaste']").get(0);
 
 	// ===== LR Creation area ====== //
@@ -132,36 +142,41 @@ function createLR() {
 
 	/* Stream Section */
 	$(LRtext).append("[hr]\n[img]http://i.imgur.com/08h88.png[/img]\n\n");
-	// Riot Twitch and YT
-	$(LRtext).append("[url=http://www.twitch.tv/team/riotgames]Twitch TV[/url]\n[url=http://www.youtube.com/lolchampseries]Youtube Stream[/url]");
-	$(LRtext).append("\n\n[hr]\n");
+	addStreams(LRtext);
 
 	/* MATCHES SECTION */
+	insertBreak(LRtext)
 	$(LRtext).append("[img]http://i.imgur.com/gsiQ0.png[/img]\n\n");
 	$(".matchDay").each(function() {
 		createMatchDayLR(LRtext, this);
 	});
 
 	/* RESULTS SECTION */
+	insertBreak(LRtext)
 	$(LRtext).append("[img]http://i.imgur.com/L6Pww.png[/img]\n\n");
 	$(".matchDay").each(function() {
-		createMatchDayResults(LRtext, this);
+		createMatchDaySpoilers(LRtext, this);
 	});
 
-	/* POLLS SECTION */
-	$(LRtext).append("\n[hr]\n[img]http://i.imgur.com/0PxXd.png[/img]\n\n");
+	/* POLLS SECTION (Duplicate of results) */
+	insertBreak(LRtext);
+	$(LRtext).append("[img]http://i.imgur.com/0PxXd.png[/img]\n\n");
 	$(".matchDay").each(function() {
-		createMatchDayPolls(LRtext, this);
+		createMatchDaySpoilers(LRtext, this);
 	});
 
 	/* VODS AND FOOTER SECTION */
-	$(LRtext).append("\n[hr]\n[img]http://i.imgur.com/QARj1.png[/img]\n\n");
+	insertBreak(LRtext);
+	$(LRtext).append("[img]http://i.imgur.com/QARj1.png[/img]\n\n");
 	$(LRtext).append("[spoiler][url=http://vods.leaguepedia.com/tournaments/]LoLVODs[/url][/spoiler][/center]\n");
 	$(LRtext).append("[hr][center][small]Thanks to Shiroiusagi for the banner.[/small][/center]");
 }
 
+/*
+Creates the "Match Day" wrapper BBCode for each day of the event
+*/
 function createMatchDayLR(LRtext, currentMatchDay) {
-	var dateVal = $("input[name='date-cntl']", currentMatchDay).val();
+	var dateVal = getMatchDate(currentMatchDay);
 	var timeVal = $("input[name='time-cntl']", currentMatchDay).val();
 	var tzVal = $("input[name='tz-cntl']", currentMatchDay).val();
 
@@ -176,15 +191,18 @@ function createMatchDayLR(LRtext, currentMatchDay) {
 	$(".match", currentMatchDay).each(function() {
 		createMatchLR(LRtext, this);
 	});
-
-	$(LRtext).append("\n[hr]\n");
 }
 
+/*
+Create the actual match BBCode
+*/
 function createMatchLR(LRtext, currentMatch) {
-	var team1 = $("#team1 option:selected", currentMatch).text();
-	var team1img = $("#team1", currentMatch).val();
-	var team2 = $("#team2 option:selected", currentMatch).text();
-	var team2img = $("#team2", currentMatch).val();
+
+	var team1 = getTeam("team1", currentMatch);
+	var team1img = getTeamImg("team1");
+	var team2 = getTeam("team2", currentMatch);
+	var team2img = getTeamImg("team2");
+
 	var gameTime = $("input[name='game-time']", currentMatch).val();
 	var gameTZ = $("input[name='game-tz']", currentMatch).val();
 	var matchURL = $("input[name='match-url']", currentMatch).val();
@@ -201,18 +219,80 @@ function createMatchLR(LRtext, currentMatch) {
 	$(LRtext).append("[spoiler=Bans and Picks]\n[/spoiler]\n\n");
 }
 
-function createMatchDayResults(LRtext, currentMatchDay) {
+/*
+Creates the spoiler BBCode for each match day (used for polls and results)
+*/
+function createMatchDaySpoilers(LRtext, currentMatchDay) {
+	var dateVal = getMatchDate(currentMatchDay);
+	$(LRtext).append("[spoiler=" + dateVal + "]\n");
 
+	$(".match", currentMatchDay).each(function() {
+		createMatchSpoilers(LRtext, this);
+	});
+
+	$(LRtext).append("[/spoiler]\n\n");
 }
 
-function createMatchResults(LRtext, currentMatch) {
+/* 
+Creates the spoiler BBCode for each match (used for polls and results)
+*/
+function createMatchSpoilers(LRtext, currentMatch) {
+	var team1 = getTeam("team1", currentMatch);
+	var team2 = getTeam("team2", currentMatch);
 
+	$(LRtext).append("[i]");
+	$(LRtext).append(team1 + " vs " + team2);
+	$(LRtext).append("[/i]\n");
+	$(LRtext).append("[spoiler][/spoiler]\n\n");
 }
 
-function createMatchDayPolls(LRtext, currentMatchDay) {
-
+/*
+Adds teams to the dropdowns (should be more dynamic)
+*/
+function addNATeams(teamDropdown) {
+	$(teamDropdown).append(createTeamOption('TSM',TSMimg));
+	$(teamDropdown).append(createTeamOption('CLG',CLGimg));
+	$(teamDropdown).append(createTeamOption('C9',C9img));
+	$(teamDropdown).append(createTeamOption('XDG',XDGimg));
+	$(teamDropdown).append(createTeamOption('EG',EGimg));
+	$(teamDropdown).append(createTeamOption('DIG',DIGimg));
+	$(teamDropdown).append(createTeamOption('CST',CSTimg));
+	$(teamDropdown).append(createTeamOption('CRS',CRSimg));
+	/* ADD OTHER TEAMS HERE */
 }
 
-function createMatchPolls(LRtext, currentMatch) {
+/*
+Add streams to the top of the LR (probably should be more dynamic)
+*/
+function addStreams(LRtext) {
+	// Riot Twitch
+	$(LRtext).append("[url=http://www.twitch.tv/team/riotgames]Twitch TV[/url]\n");
+	// Riot YT
+	$(LRtext).append("[url=http://www.youtube.com/lolchampseries]Youtube Stream[/url]\n");
+	/* ADD OTHER DEFAULT STREAMS HERE */
+}
 
+/*
+* ====================================
+*
+* Basic shortcut methods to improve
+* code legibility.
+*
+* ====================================
+*/
+
+function getMatchDate(currentMatchDay) {
+	return $("input[name='date-cntl']", currentMatchDay).val();
+}
+
+function getTeam(team, currentMatch) {
+	return $("#" + team + " option:selected", currentMatch).text();
+}
+
+function getTeamImg(team, currentMatch) {
+	return $("#" + team, currentMatch).val();
+}
+
+function insertBreak(LRtext) {
+	$(LRtext).append("\n[hr]\n");
 }
